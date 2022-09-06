1ae1cd0965a34e0cdaef66c82b56215d83ca05d5
Harrisonust <henhen.luo@gmail.com>
2022-09-06 13:09:25 +0000

    created imu msgs definition

---
+++ src/msgs/src/recv.py
@@ -0,0 +1,34 @@
+import rclpy
+from rclpy.node import Node
+
+from msgs.msg import ImuRaw        # CHANGE
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            ImuRaw,                                              # CHANGE
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription
+
+    def listener_callback(self, msg):
+            self.get_logger().info('I heard: "%f"' % msg.linear_acc[0]) # CHANGE
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/msgs/src/send.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+
+from msgs.msg import ImuRaw   
+
+
+class MinimalPublisher(Node):
+
+    def __init__(self):
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(ImuRaw, 'IMU_test', 10)    
+        timer_period = 0.5
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = ImuRaw()                                          
+        msg.linear_acc = [0.1, 0.2, 0.3]                                     
+        msg.rotational_acc = [0.1, 0.2, 0.3]                                     
+        self.publisher_.publish(msg)
+        self.get_logger().info(f'Publishing: [{msg.linear_acc[0]}, {msg.linear_acc[1]}, {msg.linear_acc[2]}]') 
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_publisher = MinimalPublisher()
+
+    rclpy.spin(minimal_publisher)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
