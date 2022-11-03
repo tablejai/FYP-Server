99c7ad82c7b78825af323f817c875cc1281ada48
FYPGroup <gloveGroup@gmail.com>
2022-11-03 15:51:55 +0000

    Receiver Module

--- setup.sh
+++ setup.sh
@@ -1,7 +1,20 @@
 sudo apt update
 
+# Add ROS-Foxy repository public key
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+# Add repository to source list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# Install dependencies for foxy and foxy itself
 sudo apt install keychain python3-pip ros-foxy-tf-transformations ros-foxy-rqt ros-foxy-rqt-common-plugins -y
 sudo pip install transforms3d
+
+# Install colcon for python
+sudo apt install python3-colcon-common-extensions
+
+# Install flask
+python3 -m pip install flask
 
 # reinit rqt
 rm ~/.config/ros.org/rqt_gui.ini
---
+++ src/receiver/receiver/main.py
@@ -0,0 +1,54 @@
+import rclpy
+import json
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+from flask import Flask, request, jsonify
+from flask_classful import FlaskView, route
+
+class MinimalPublisher(Node):
+    def __init__(self):
+        rclpy.init(args=None)
+        super().__init__('minimal_publisher')
+        self.publisher_ = self.create_publisher(String, 'topic', 10)
+
+    def publishData(self, data):
+        msg = String()
+        msg.data = data
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+class HelloView(FlaskView):
+    app = Flask("Test")
+    publisher= MinimalPublisher()
+
+    @route('/',methods=['POST'], strict_slashes=False)
+    def post(self):
+        content = json.loads(request.data)
+        imu0 = content["imu0"]
+        imu0_ax = imu0['ax']
+        imu0_ay = imu0['ay']
+        imu0_az = imu0['az']
+        HelloView.publisher.publishData(str(imu0_ax))
+        return 'Hi'
+
+def main(args=None):
+    HelloView.register(HelloView.app)
+    HelloView.app.run(host='0.0.0.0', port=5001, debug=True)
+
+
+    rclpy.spin(HelloView.publisher)
+
+    HelloView.publisher.init()
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
+
