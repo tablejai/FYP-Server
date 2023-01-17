91cc9d88af3cb50efb23552fdd191b02557f5003
Harrisonust <henhen.luo@gmail.com>
2023-01-17 10:03:23 +0000

    launch file pass

--- src/launch_master/launch/launch_master.launch.py
+++ src/launch_master/launch/launch_master.launch.py
@@ -3,6 +3,7 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import os
+from ament_index_python import get_package_share_directory
 
 def generate_launch_description():
     return LaunchDescription([
@@ -20,7 +21,7 @@ def generate_launch_description():
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 os.path.join(
-                    get_package_share_directory('dynamic_transform_calculator'),
-                    'launch/calculator.launch.py'))
+                    get_package_share_directory('dtf_calculator'),
+                    'launch/dtf_calculator.launch.py'))
         ),
     ])
