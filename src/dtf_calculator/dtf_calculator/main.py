8e027a13b0cca01ffe41aa593941093caa3bf8a5
Harrisonust <henhen.luo@gmail.com>
2023-01-17 06:44:13 +0000

    only consider rpy

--- src/dtf_calculator/dtf_calculator/main.py
+++ src/dtf_calculator/dtf_calculator/main.py
@@ -9,6 +9,7 @@ from sensor_msgs.msg import Imu
 
 import tf_transformations
 import numpy as np
+from math import cos, sin, pi, sqrt, atan2, asin
  
 def quaternion_rotation_matrix(Q):
     """
@@ -59,83 +60,38 @@ class DTF_Calculator(Node):
         self.tf_broadcaster = TransformBroadcaster(self)
 
         # Create a subscriber to the topic /Imu
-        self.imu_msg_filter_a = message_filters.Subscriber(self, Imu, '/Imu0')
-        self.imu_msg_filter_b = message_filters.Subscriber(self, Imu, '/Imu1')
+        self.imu_msg_filter_0 = message_filters.Subscriber(self, Imu, '/Imu0')
+        self.imu_msg_filter_1 = message_filters.Subscriber(self, Imu, '/Imu1')
+        self.imu_msg_filter_2 = message_filters.Subscriber(self, Imu, '/Imu2')
 
-        self.ats = message_filters.TimeSynchronizer([self.imu_msg_filter_a, self.imu_msg_filter_b], 10)
-        self.ats.registerCallback(self.publish_dtf)
+        self.ats1 = message_filters.TimeSynchronizer([self.imu_msg_filter_0, self.imu_msg_filter_1], 10)
+        self.ats2 = message_filters.TimeSynchronizer([self.imu_msg_filter_0, self.imu_msg_filter_2], 10)
+        self.ats1.registerCallback(self.publish_dtf)
+        self.ats2.registerCallback(self.publish_dtf)
 
-        self.vel_x = 0
-        self.vel_y = 0
-        self.vel_z = 0
-        self.pos_x = 0
-        self.pos_y = 0
-        self.pos_z = 0
-        self.previous_tick = 0
-        self.current_tick = 0
-        self.first_iter = True
+        self.finger_length = 2.0
 
     # TODO: take origin tf into account
-    def publish_dtf(self, msg1, msg2):
-
-        print("\n\n==================\nmsg received")
-
-        # Calculate the time difference between the current and previous IMU messages
-        self.current_tick = msg1.header.stamp.sec + msg1.header.stamp.nanosec * 1e-9
-        dt = self.current_tick - self.previous_tick
-
-        if self.current_tick < self.previous_tick:
-            return
-
-        if self.first_iter:
-            self.previous_tick = msg1.header.stamp.sec + msg1.header.stamp.nanosec * 1e-9
-            self.first_iter = False
-            return
-
+    def publish_dtf(self, imu_parent, imu_child):
         tf = TransformStamped()
-        tf.header.stamp = msg1.header.stamp
-        tf.header.frame_id = msg1.header.frame_id
-        tf.child_frame_id = msg2.header.frame_id
+        tf.header.stamp = imu_parent.header.stamp
+        tf.header.frame_id = imu_parent.header.frame_id
+        tf.child_frame_id = imu_child.header.frame_id
         
-        # Get linear acceleration from IMU data
-        acc1 = np.array([msg1.linear_acceleration.x, msg1.linear_acceleration.y, msg1.linear_acceleration.z])
-        acc2 = np.array([msg2.linear_acceleration.x, msg2.linear_acceleration.y, msg2.linear_acceleration.z])
+        # rpy from quaternion
+        roll, pitch, yaw = tf_transformations.euler_from_quaternion(
+            np.array([imu_parent.orientation.x, imu_parent.orientation.y, imu_parent.orientation.z, imu_parent.orientation.w]) - 
+            np.array([imu_child.orientation.x, imu_child.orientation.y, imu_child.orientation.z, imu_child.orientation.w])
+        )
 
-        rpy1 = tf_transformations.euler_from_quaternion([msg1.orientation.x, msg1.orientation.y, msg1.orientation.z, msg1.orientation.w])
-        rpy2 = tf_transformations.euler_from_quaternion([msg2.orientation.x, msg2.orientation.y, msg2.orientation.z, msg2.orientation.w])
-        print(f"{msg1.header.frame_id} acc: {acc1} orientation: {rpy1}")
-        print(f"{msg2.header.frame_id} acc: {acc2} orientation: {rpy2}")
-        
-        # Convert the linear acceleration to the global reference frame
-        acc1 = np.dot(quaternion_rotation_matrix(msg1.orientation), acc1)
-        acc2 = np.dot(quaternion_rotation_matrix(msg2.orientation), acc2)
-        
-        print("rotation matrix applied")
-        print(f"{msg1.header.frame_id} acc:  ", acc1)
-        print(f"{msg2.header.frame_id} acc:  ", acc2)
-
-        # Calculate the difference between the two accelerations
-        acc_x, acc_y, acc_z = acc1 - acc2
-
-        # Integrate acceleration to estimate velocity
-        self.vel_x += acc_x * dt
-        self.vel_y += acc_y * dt
-        self.vel_z += acc_z * dt
-
-        # Integrate velocity to estimate position
-        self.pos_x += self.vel_x * dt
-        self.pos_y += self.vel_y * dt
-        self.pos_z += self.vel_z * dt
-
-        # Set the transform's translation
-        tf.transform.translation.x = self.pos_x
-        tf.transform.translation.y = self.pos_y
-        tf.transform.translation.z = self.pos_z
+        # Calculate the rotation matrix from quaternion
+        tf.transform.translation.x = self.finger_length * cos(pitch) * cos(yaw) 
+        tf.transform.translation.y = self.finger_length * cos(pitch) * sin(yaw)
+        tf.transform.translation.z = self.finger_length * sin(pitch)
 
         # Send the transform
         self.tf_broadcaster.sendTransform(tf)
-        print("tf:", tf.transform.translation)
-        self.previous_tick = self.current_tick
+        print(f"tf {tf.header.frame_id} -> {tf.child_frame_id} sent({tf.transform.translation})")
 
 def main():
     rclpy.init()
