3c3d2586741a940fe445e13c9b6352eeb7a7799e
Harrisonust <henhen.luo@gmail.com>
2023-01-18 06:52:54 +0000

    update detetctor

--- src/detector/detector/gesture_detection.py
+++ src/detector/detector/gesture_detection.py
@@ -1,9 +1,38 @@
 import rclpy
 from rclpy.node import Node
 
+from std_msgs.msg import String
+from sensor_msgs.msg import Imu
+from sensor_msgs.msg import MagneticField
+
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
+import message_filters
+
 class Detector(Node):
     def __init__(self):
         super().__init__('Detector')
+
+        # create subscribers
+        self.imu_subscribers = []
+        self.imu_mag_subscribers = []
+        for i in range(3):
+            self.imu_subscribers.append(message_filters.Subscriber(self, Imu, '/Imu'+str(i)))
+            self.imu_mag_subscribers.append(message_filters.Subscriber(self, MagneticField, '/Imu'+str(i)+'/Mag'))
+        
+        self.syncer = message_filters.ApproximateTimeSynchronizer([fs for fs in self.imu_subscribers + self.imu_mag_subscribers], 10, 0.1)
+        self.syncer.registerCallback(self.callback)
+
+        # create a transform listener
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
+
+        # create a publisher
+        self.command_publisher = self.create_publisher(String, '/Command', 10)
+    
+    def callback(self, *args):
+        self.command_publisher.publish(String(data='hello'))
 
 def main():
     rclpy.init()
