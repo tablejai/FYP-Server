ab29f99671d612210664b0f99ed2a0906ad0e6e1
Harrisonust <henhen.luo@gmail.com>
2023-01-21 03:30:41 +0000

    include tf message to training data

--- src/labeler/labeler/labeler.py
+++ src/labeler/labeler/labeler.py
@@ -1,29 +1,51 @@
 import rclpy
 from rclpy.node import Node
+import tf2_ros
 
 # msg
 import message_filters
+from message_filters import SimpleFilter
 
 # msg
 from sensor_msgs.msg import Imu
+from tf2_msgs.msg import TFMessage
 
 # system
 import threading
 import pandas as pd
 import sys
 
+class TfSubscriber(SimpleFilter):
+    def __init__(self, node, message_type, topic, from_frame, to_frame):
+        SimpleFilter.__init__(self)
+        self.node = node
+        self.message_type = message_type
+        self.topic = topic 
+        self.from_frame = from_frame
+        self.to_frame = to_frame
+        self.sub = self.node.create_subscription(self.message_type, self.topic, self.callback, 10)
+
+    def callback(self, msg):
+        for tf in msg.transforms:
+            if tf.header.frame_id == self.from_frame and tf.child_frame_id == self.to_frame:
+                self.signalMessage(tf)
+
 class Labeler(Node):
 
     def __init__(self, data_path):
         super().__init__('Labeler')
-        self.imu_list = ["/Imu0", "/Imu1", "/Imu2"]
+        self.imu_list = ["Imu0", "Imu1", "Imu2"]
+        self.tf_list = ["imu0_to_imu1", "imu0_to_imu2"]
         self.data_path = data_path
 
         # create imu subscribers
-        self.imu_msg_filter_0 = message_filters.Subscriber(self, Imu, '/Imu0')
-        self.imu_msg_filter_1 = message_filters.Subscriber(self, Imu, '/Imu1')
-        self.imu_msg_filter_2 = message_filters.Subscriber(self, Imu, '/Imu2')
-        self.syncer = message_filters.TimeSynchronizer([self.imu_msg_filter_0, self.imu_msg_filter_1, self.imu_msg_filter_2], 10)
+        self.imu_msg_filter0 = message_filters.Subscriber(self, Imu, '/Imu0')
+        self.imu_msg_filter1 = message_filters.Subscriber(self, Imu, '/Imu1')
+        self.imu_msg_filter2 = message_filters.Subscriber(self, Imu, '/Imu2')
+        self.tf_msg_filter1 = TfSubscriber(self, TFMessage, '/tf', from_frame='imu0', to_frame='imu1')
+        self.tf_msg_filter2 = TfSubscriber(self, TFMessage, '/tf', from_frame='imu0', to_frame='imu2')
+        
+        self.syncer = message_filters.TimeSynchronizer([self.imu_msg_filter0, self.imu_msg_filter1, self.imu_msg_filter2, self.tf_msg_filter1, self.tf_msg_filter2], 10)
         self.syncer.registerCallback(self.cb)
 
         # create label list
@@ -40,6 +62,14 @@ class Labeler(Node):
             self.data[f"{imu}_orientation_y"] = []
             self.data[f"{imu}_orientation_z"] = []
             self.data[f"{imu}_orientation_w"] = []
+        for tf in self.tf_list:
+            self.data[f"{tf}_translation_x"] = []
+            self.data[f"{tf}_translation_y"] = []
+            self.data[f"{tf}_translation_z"] = []
+            self.data[f"{tf}_rotation_x"] = []
+            self.data[f"{tf}_rotation_y"] = []
+            self.data[f"{tf}_rotation_z"] = []
+            self.data[f"{tf}_rotation_w"] = []
 
         # create new thread to save data
         self.thread = threading.Thread(target=self.save_data)
@@ -69,7 +99,7 @@ Enter labels: ''').strip().split(" ")
         print(f"Data length: {len(self.data_df)}")
         print(f"Label length: {label}")
 
-    def cb(self, msg0, msg1, msg2):
+    def cb(self, msg0, msg1, msg2, msg3, msg4):
         self.data[f"timestamp"].append(msg0.header.stamp.sec + msg0.header.stamp.nanosec*1e-9)
         for imu, msg in zip(self.imu_list, [msg0, msg1, msg2]):
             self.data[f"{imu}_linear_accleration_x"].append(msg.linear_acceleration.x)
@@ -82,7 +112,16 @@ Enter labels: ''').strip().split(" ")
             self.data[f"{imu}_orientation_y"].append(msg.orientation.y)
             self.data[f"{imu}_orientation_z"].append(msg.orientation.z)
             self.data[f"{imu}_orientation_w"].append(msg.orientation.w)
-        
+
+        for tf, msg in zip(self.tf_list, [msg3, msg4]):
+            self.data[f"{tf}_translation_x"].append(msg.transform.translation.x)
+            self.data[f"{tf}_translation_y"].append(msg.transform.translation.y)
+            self.data[f"{tf}_translation_z"].append(msg.transform.translation.z)
+            self.data[f"{tf}_rotation_x"].append(msg.transform.rotation.x)
+            self.data[f"{tf}_rotation_y"].append(msg.transform.rotation.y)
+            self.data[f"{tf}_rotation_z"].append(msg.transform.rotation.z)
+            self.data[f"{tf}_rotation_w"].append(msg.transform.rotation.w)
+
 def main(args=None):
     rclpy.init(args=args)
 
