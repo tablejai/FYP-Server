7cbaa918e0ab24962c9fe2995a3af74398e2e904
Harrisonust <henhen.luo@gmail.com>
2023-02-02 04:03:20 +0000

    update setup script(add pyautogui)

--- setup.sh
+++ setup.sh
@@ -40,7 +40,10 @@ python3 -m pip install transforms3d rosbags
 python3 -m pip install Flask==2.1.2 Flask-Classful==0.14.2 Werkzeug==2.1.2 
 
 # Install pandas
-python3 -m pip install tensorflow pandas numpy
+python3 -m pip install tensorflow pandas numpy pyautogui
 
 # reinit rqt
-rm ~/.config/ros.org/rqt_gui.ini
+file="~/.config/ros.org/rqt_gui.ini"
+if [ -f "$file" ] ; then
+    rm "$file"
+fi
