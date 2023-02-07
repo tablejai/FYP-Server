a6bd9af392db3206c1b403c5c5c3a3e69d362b02
Harrisonust <henhen.luo@gmail.com>
2023-02-07 10:39:10 +0000

    WIP

--- src/commander/commander/control.py
+++ src/commander/commander/control.py
@@ -3,18 +3,40 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 from msgs.msg import Geasture
+import pyautogui
 
 class Commander(Node):
-
     def __init__(self):
         super().__init__('Commander')
-
-        # Create a subscriber
         self.subscriber = self.create_subscription(Geasture, '/Geastures', self.callback, 10)
-    
+        self.get_logger.info(pyautogui.position())
+
     def callback(self, msg):
-        print("receive:", msg.type)
-        
+        if msg.type == Geasture.STATIC:    
+            self.get_logger().info("STATIC")
+        elif msg.type == Geasture.SLIDE_UP:
+            self.get_logger().info("SLIDE_UP")
+        elif msg.type == Geasture.SLIDE_DOWN:
+            self.get_logger().info("SLIDE_DOWN")
+        elif msg.type == Geasture.SLIDE_LEFT:
+            self.get_logger().info("SLIDE_LEFT")
+        elif msg.type == Geasture.SLIDE_RIGHT:
+            self.get_logger().info("SLIDE_RIGHT")
+        elif msg.type == Geasture.ZOOM_IN:
+            self.get_logger().info("ZOOM_IN")
+        elif msg.type == Geasture.ZOOM_OUT:
+            self.get_logger().info("ZOOM_OUT")
+        elif msg.type == Geasture.HIGHLIGHT:
+            self.get_logger().info("HIGHLIGHT")
+        elif msg.type == Geasture.ON_YES:
+            self.get_logger().info("ON_YES")
+        elif msg.type == Geasture.OFF_NO:
+            self.get_logger().info("OFF_NO")
+        elif msg.type == Geasture.END:
+            self.get_logger().info("END")
+        else:
+            pass
+
 def main(args=None):
     rclpy.init()
     node = Commander()
