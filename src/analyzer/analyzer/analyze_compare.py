67c578c508a52e534fc2cb61195142b424a615a3
Harrisonust <henhen.luo@gmail.com>
2023-03-13 06:49:42 +0000

    minor

--- src/analyzer/analyzer/analyze_compare.py
+++ src/analyzer/analyzer/analyze_compare.py
@@ -5,16 +5,16 @@ import utils
 
 # Load the data
 dataset = utils.load_data()
-target_gesture = "SLIDE_UP"
+target_gesture = "STATIC"
 target_data = dataset[target_gesture]
 num_data = len(target_data)
 print(f"Number of data samples for {target_gesture}: {num_data}")
 
 # Define helper functions
-def plot_data(ax_list, data_list, title_list, color, linestyle="solid"):
+def plot_data(ax_list, data_list, title_list, color, linestyle="solid", marker=""):
     lines = []
     for ax, data, title in zip(ax_list, data_list, title_list):
-        lines += ax.plot(data, color=color, linestyle=linestyle)
+        lines += ax.plot(data, color=color, linestyle=linestyle, marker=marker)
         ax.set_title(title)
         ax.set_ylim([-1.5 * 9.8, 1.5 * 9.8] if 'acc' in title else [-5, 5])
     return lines
@@ -45,6 +45,7 @@ def switch_plot(event):
     global file_path1
     global plot_index
     global lines
+    global num_data
     
     if event.key != 'up' and event.key != 'down' and event.key != 'left' and event.key != 'right':
         return
@@ -59,7 +60,8 @@ def switch_plot(event):
         plot_index -= 1
     elif event.key == 'down' or event.key == 'right':
         plot_index += 1
-
+    plot_index %= num_data
+        
     file_path2 = target_data[plot_index]
     raw_data2 = pd.read_csv(f'./rosbag/data/data_clean/{file_path2}_data.csv')
     raw_data2['timestamp'] = raw_data2['timestamp'] - raw_data2['timestamp'][0]
@@ -67,12 +69,12 @@ def switch_plot(event):
     acc_data2 = [raw_data2[f'Imu{i}_linear_accleration_{xyz}'] for i in range(3) for xyz in ['x', 'y', 'z']]
     vel_data2 = [raw_data2[f'Imu{i}_angular_velocity_{xyz}'] for i in range(3) for xyz in ['x', 'y', 'z']]
 
-    lines = plot_data(acc_axes, acc_data2, acc_titles, "red", linestyle="dashed")
-    lines += plot_data(vel_axes, vel_data2, vel_titles, "red", linestyle="dashed")
+    lines = plot_data(acc_axes, acc_data2, acc_titles, "red", linestyle="dotted")
+    lines += plot_data(vel_axes, vel_data2, vel_titles, "red", linestyle="dotted")
     
 
     fig.legend([f'{file_path1}', f'{file_path2}'])
-    fig.suptitle(f'Compare({target_gesture}): {file_path1} <-> {file_path2} ')
+    fig.suptitle(f'Compare({target_gesture} {plot_index}/{num_data-1}): {file_path1} <-> {file_path2} ')
     plt.draw()
 
 plt.connect('key_press_event', switch_plot)
