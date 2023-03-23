8db39ac91e52aba57423e63db079dbdcde38b7fb
Tablejai <alexchan815@gmail.com>
2023-03-23 07:37:52 +0000

    time limit control in 1 second

--- src/commander/commander/control.py
+++ src/commander/commander/control.py
@@ -1,30 +1,37 @@
 import rclpy
 from rclpy.node import Node
 
+from datetime import datetime, timedelta
 from std_msgs.msg import String
 from msgs.msg import Geasture
 import requests
 import json
-
-LOCAL_IP = "192.168.0.104"
+LOCAL_IP = "10.89.10.144"
 LOCAL_PORT = "7000"
+
+command_list = ["STATIC", "SLIDE_UP", "SLIDE_DOWN", "SLIDE_LEFT", "SLIDE_RIGHT", "RELEASE", "GRASP", "HIGHLIGHT","ON_YES","OFF_NO","NONE"]
 
 class Commander(Node):
 
     def __init__(self):
         super().__init__('Commander')
-
         # Create a subscriber
+        self.url = f"http://{LOCAL_IP}:{LOCAL_PORT}"
         self.subscriber = self.create_subscription(Geasture, '/Geastures', self.callback, 10)
+        command_state_init_time = datetime.now() - timedelta(seconds= 1)
+        self.command_states = {command: command_state_init_time for command in command_list}
     
     def callback(self, msg):
-        self.send_request(msg.type)
+        print(f"command received: {msg.type}")
+        self.command_ctrl_flow(msg.type)
 
-    def send_request(self, msg):
-        send_json = {"command": msg}
-        url = f"http://{LOCAL_IP}:{LOCAL_PORT}"
-        requests.post(url, json=send_json)
-        print("sent msg")
+    def command_ctrl_flow(self, command):
+        print(self.command_states)
+        cur_command = command_list[command]
+        if datetime.now() - self.command_states[cur_command] > timedelta(seconds=1):
+            json_to_sent = {"command": command}
+            requests.post(self.url, json=json_to_sent)
+            self.command_states[cur_command] = datetime.now()
         
 def main(args=None):
     rclpy.init()
