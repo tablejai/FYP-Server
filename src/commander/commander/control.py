ec93a098615d63ab704b35fd54cf5001f7483640
Tablejai <42264922+tablejai@users.noreply.github.com>
2023-04-17 19:31:39 +0800

    Device interface and command loading the json

--- src/commander/commander/control.py
+++ src/commander/commander/control.py
@@ -6,37 +6,71 @@ from std_msgs.msg import String, Float32
 from msgs.msg import Gesture
 import requests
 import json
+
 LOCAL_IP = "host.docker.internal"
 LED_IP = "192.168.4.3"
 LOCAL_PORT = "7000"
+dev_config_list = []
 
-command_list = ["STATIC", "SLIDE_UP", "SLIDE_DOWN", "SLIDE_LEFT",
-                "SLIDE_RIGHT", "RELEASE", "GRASP", "HIGHLIGHT", "ON_YES", "OFF_NO", "NONE"]
+command_list = [
+    "STATIC",
+    "SLIDE_UP",
+    "SLIDE_DOWN",
+    "SLIDE_LEFT",
+    "SLIDE_RIGHT",
+    "RELEASE",
+    "GRASP",
+    "HIGHLIGHT",
+    "ON_YES",
+    "OFF_NO",
+    "NONE",
+]
 
 
 class Commander(Node):
-
     def __init__(self):
-        super().__init__('Commander')
+        super().__init__("Commander")
         # Create a subscriber
-        self.url = f"http://{LOCAL_IP}:{LOCAL_PORT}"
-        self.led_url = f"http://{LED_IP}"
+        self.load_dev_config()
+        self.url = "http://{}:7000"
+        self.led_url = "http://{}"
         self.gestures_subscriber = self.create_subscription(
-            Gesture, '/Gestures', self.gesture_callback, 10)
+            Gesture, "/Gestures", self.gesture_callback, 10
+        )
         self.orientation_subscriber = self.create_subscription(
-            Float32, '/Orientation', self.orientation_callback, 10)
+            Float32, "/Orientation", self.orientation_callback, 10
+        )
         self.init_angle = None
         self.face_ppt = None
+        self.send_ip = "192.168.4.1"
 
         command_state_init_time = datetime.now() - timedelta(seconds=1)
         self.command_states = {
-            command: command_state_init_time for command in command_list}
+            command: command_state_init_time for command in command_list
+        }
         self.get_logger().info(f"aksdfss")
 
+    def load_dev_config(self):
+        global dev_config_list
+        with open(
+            "/home/ubuntu/FYP-ROS/src/commander/commander/dev_config.json"
+        ) as dev_config_file:
+            dev_dict = json.load(dev_config_file)
+            for dev_item in dev_dict.values():
+                u_bound = dev_item["upper_bound"]
+                l_bound = dev_item["lower_bound"]
+                dev_type = dev_item["device_type"]
+                dev_config_obj = {
+                    "lower_bound": float(l_bound),
+                    "upper_bound": float(u_bound),
+                    "dev_type": dev_type,
+                    "exceed_0": True if float(l_bound) > float(u_bound) else False,
+                    "exceed_360": True if float(l_bound) < float(u_bound) else False,
+                    "ip_address": dev_item["ip_address"],
+                }
+                dev_config_list.append(dev_config_obj)
 
     def gesture_callback(self, msg):
-        self.get_logger().info(f"command received: {msg.type}")
-        self.get_logger().info(f"{self.face_ppt=}")
         if self.face_ppt is True:
             self.ppt_ctrl_flow(msg.type)
         elif self.face_ppt is False:
@@ -44,51 +78,54 @@ class Commander(Node):
 
     def orientation_callback(self, msg):
         # TODO: See how to get the proper angle
-        # self.get_logger().info(f"{self.init_angle=}")
-        # self.get_logger().info(f"{msg.data=}")
+        send_dev = None
+        send_ip = None
+        global dev_config_list
         angle = int(msg.data)
-        if self.init_angle is None:
-            self.init_angle = angle
-            if self.init_angle >= 90 and self.init_angle <= 270:
-                self.exceed_0 = False
-                self.exceed_360 = False
-                self.lower_bound = angle - 90
-                self.upper_bound = angle + 90
-            elif self.init_angle < 90:
-                self.exceed_0 = True
-                self.exceed_360 = False
-                self.lower_bound = 270 + angle
-                self.upper_bound = angle + 90
-            elif self. init_angle > 270:
-                self.exceed_0 = False
-                self.exceed_360 = True
-                self.lower_bound = angle - 90
-                self.upper_bound = angle - 270
+        for dev in dev_config_list:
+            lower_bound = dev["lower_bound"]
+            upper_bound = dev["upper_bound"]
+            exceed_0 = dev["exceed_0"]
+            exceed_360 = dev["exceed_360"]
+            if not exceed_0 and not exceed_360:
+                if angle >= lower_bound and angle >= upper_bound:
+                    send_dev = dev["dev_type"]
+                    send_ip = dev["ip_address"]
+                    break
+            elif not exceed_0 and exceed_360:
+                if (angle >= lower_bound and angle <= 360) or angle <= lower_bound:
+                    send_dev = dev["dev_type"]
+                    send_ip = dev["ip_address"]
+                    break
+            elif exceed_0 and not exceed_360:
+                if (angle <= upper_bound and angle >= 0) or angle >= upper_bound:
+                    send_dev = dev["dev_type"]
+                    send_ip = dev["ip_address"]
+                    break
+        self.get_logger().info(f"send_dev: {send_dev}")
+        self.get_logger().info(f"device_ip: {send_ip}")
+        if send_dev == "Powerpoint":
+            self.face_ppt = True
+            self.send_ip = send_ip
         else:
-            if not self.exceed_0 and not self.exceed_360:
-                self.face_ppt = angle > self.lower_bound and angle < self.upper_bound
-            elif not self.exceed_0 and self.exceed_360:
-                self.face_ppt = (angle > self.lower_bound and angle <
-                                 360) or angle < self.lower_bound
-            elif self.exceed_0 and not self.exceed_360:
-                self.face_ppt = (angle < self.upper_bound and angle >
-                                 0) or angle > self.upper_bound
+            self.face_ppt = False
+            self.send_ip = send_ip
 
     def ppt_ctrl_flow(self, command):
-        print(self.command_states)
         cur_command = command_list[command]
         if datetime.now() - self.command_states[cur_command] > timedelta(seconds=2):
             json_to_sent = {"command": command}
-            requests.post(self.url, json=json_to_sent)
+            requests.post(self.url.format(self.send_ip), json=json_to_sent)
             self.command_states[cur_command] = datetime.now()
 
     def led_ctrl_flow(self, command):
-        print(self.command_states)
         cur_command = command_list[command]
         if datetime.now() - self.command_states[cur_command] > timedelta(seconds=1.3):
             if cur_command == "RELEASE" or cur_command == "GRASP":
                 requests.get(
-                    self.led_url, params="ON" if cur_command == "RELEASE" else "OFF")
+                    self.led_url.format(self.send_ip),
+                    params="ON" if cur_command == "RELEASE" else "OFF",
+                )
             self.command_states[cur_command] = datetime.now()
 
 
@@ -101,5 +138,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
---
+++ src/commander/commander/interface.py
@@ -0,0 +1,210 @@
+import tkinter as tk
+from tkinter import Button, Toplevel, ttk
+import math
+import json
+import pandas as pd
+import os
+from functools import partial
+
+root = tk.Tk("Smart Glove Device Configuration")
+root.title("Device Position Confirguration Interface")
+
+dev_button_list = dict()
+CONFIG_FILE_NAME = "dev_config.json"
+DEFAULT_DEV_NAME = "dev"
+DEVICE_TYPES = ["LED", "PowerPoint"]
+
+# TODO: deal with issue where device has same name or same IP
+
+
+class InterfaceCanvas():
+    def __init__(self):
+        self.width = 600
+        self.height = 600
+        self.canvas = tk.Canvas(root, width=self.width, height=self.height)
+        border = self.canvas.create_oval(self.width / 3, self.height / 3,
+                                         self.width * 2 / 3, self.height * 2 / 3)
+        centre = self.canvas.create_oval(self.width * 6 / 13, self.height * 6 / 13,
+                                         self.width * 7 / 13, self.height * 7 / 13, fill="red")
+        self.centre_x = self.width / 2
+        self.centre_y = self.height / 2
+        self.radius = self.width / 6
+        self.add_labeled_text()
+        add_btn = Button(root, text="Add device", width=19, height=2,
+                         bd="10", command=self.create_new_device)
+        add_btn.place(x=70, y=80)
+
+        remove_btn = Button(root, text="Remove device", width=19, height=2,
+                            bd="10", command=self.remove_last_object)
+        remove_btn.place(x=310, y=80)
+        export_btn = Button(root, text="Export Configuration", width=45, height=2,
+                            bd="10", command=self.export_dev_json)
+        export_btn.place(x=70, y=450)
+        self.load_dev_json()
+        self.canvas.pack()
+
+    def add_labeled_text(self):
+        self.canvas.create_text(self.centre_x,
+                                self.centre_y, text="Glove")
+        self.canvas.create_text(
+            self.centre_x, self.centre_y - self.radius - 30, text="North (0)")
+        self.canvas.create_text(
+            self.centre_x, self.centre_y + self.radius + 30, text="South (180)")
+        self.canvas.create_text(
+            self.centre_x + self.radius + 60, self.centre_y, text="East (90)")
+        self.canvas.create_text(
+            self.centre_x - self.radius - 60, self.centre_y, text="West (270)")
+
+    def create_new_device(self, name=DEFAULT_DEV_NAME):
+        if name == DEFAULT_DEV_NAME:
+            name += str(len(dev_button_list.keys()))
+        edit_dev_pair = partial(self.edit_dev_config, name)
+        dev_btn = Button(self.canvas, text=name, width=2, height=2,
+                         bd="0", command=edit_dev_pair)
+        dev_button_list[name] = {"instance": dev_btn}
+        dev_button_list[name]["device_type"] = DEVICE_TYPES[0]
+        dev_button_list[name]["ip_address"] = "0.0.0.0"
+        self.refresh_dev_bt_pos()
+
+    def remove_last_object(self):
+        global dev_button_list
+        if len(dev_button_list) == 0:
+            return
+        dev_to_delete = list(dev_button_list.keys())[-1]
+        dev_button_list[dev_to_delete]["instance"].destroy()
+        dev_button_list.pop(dev_to_delete)
+        self.refresh_dev_bt_pos()
+
+    def edit_dev_config(self, dev_name):
+        edit_window = Toplevel(root)
+        edit_window.title("Editing " + dev_name)
+        edit_window.geometry("300x180")
+
+        # Device Name
+        cur_row = 0
+        tk.Label(edit_window, text="Device Name").grid(row=cur_row)
+        device_name_entry = tk.Entry(edit_window)
+        device_name_entry.grid(row=cur_row, column=1)
+        device_name_entry.insert(tk.END, dev_name)
+
+        # Device Type
+        cur_row += 1
+        tk.Label(edit_window, text="Device Type").grid(row=cur_row)
+        device_type_combo_box = ttk.Combobox(
+            edit_window, state="readonly", values=DEVICE_TYPES)
+        device_type_combo_box.current(DEVICE_TYPES.index(
+            dev_button_list[dev_name]["device_type"]))
+        device_type_combo_box.grid(row=cur_row, column=1)
+
+        # Lower Bound
+        cur_row += 1
+        tk.Label(edit_window, text="Start Angle").grid(row=cur_row)
+        upper_bound_entry = tk.Entry(edit_window)
+        upper_bound_entry.insert(
+            tk.END, dev_button_list[dev_name]["upper_bound"])
+        upper_bound_entry.grid(row=cur_row, column=1)
+
+        # Upper Bound Bound
+        cur_row += 1
+        tk.Label(edit_window, text="End Angle").grid(row=cur_row)
+        lower_bound_entry = tk.Entry(edit_window)
+        lower_bound_entry.insert(
+            tk.END, dev_button_list[dev_name]["lower_bound"])
+        lower_bound_entry.grid(row=cur_row, column=1)
+
+        cur_row += 1
+        tk.Label(edit_window, text="IP Address").grid(row=cur_row)
+        ip_address_entry = tk.Entry(edit_window)
+        ip_address_entry.insert(
+            tk.END, dev_button_list[dev_name]["ip_address"])
+        ip_address_entry.grid(row=cur_row, column=1)
+
+        # Save Button
+        cur_row += 1
+        save_partial = partial(self.save_dev_settings, edit_window, dev_name, device_name_entry,
+                               device_type_combo_box, upper_bound_entry, lower_bound_entry, ip_address_entry)
+        save_button = Button(edit_window, text="Save",
+                             width=4, height=2, bd="0", command=save_partial)
+        save_button.grid(row=cur_row, column=0)
+
+        # Quit Button
+        save_button = Button(edit_window, text="Quit",
+                             width=4, height=2, bd="0", command=edit_window.destroy)
+        save_button.grid(row=cur_row, column=1)
+
+    def save_dev_settings(self, edit_window, dev_name, device_name_entry, device_type_combo_box, upper_bound_entry, lower_bound_entry, ip_address_entry):
+        global dev_button_list
+        dev_button_list[dev_name]["instance"].destroy()
+        dev_button_list.pop(dev_name)
+        # self.create_new_device(device_name_entry.get())
+        print(f"{upper_bound_entry.get()=}, {lower_bound_entry.get()=}")
+        dev_button_list[device_name_entry.get()] = {
+            "upper_bound": upper_bound_entry.get(),
+            "lower_bound": lower_bound_entry.get(),
+            "device_type": device_type_combo_box.get(),
+            "ip_address": ip_address_entry.get()
+        }
+        edit_window.destroy()
+        self.refresh_dev_bt_pos(auto_gen=False)
+
+    def destroy_all_dev_btn(self):
+        for index, btn in enumerate(dev_button_list.values()):
+            if "instance" in btn:
+                btn["instance"].destroy()
+                btn.pop("instance")
+
+    def refresh_dev_bt_pos(self, auto_gen=True):
+        angle_divider = len(dev_button_list.keys())
+        if angle_divider == 0:
+            return
+        angle_per_step = 360 / angle_divider
+        self.destroy_all_dev_btn()
+
+        for index, btn in enumerate(dev_button_list.values()):
+            if auto_gen:
+                cur_angle = angle_per_step * index
+                btn["upper_bound"] = str(cur_angle + angle_per_step / 2)
+                btn["lower_bound"] = str(
+                    (cur_angle - angle_per_step / 2) % 360)
+            else:
+                cur_angle = (int(float(btn["upper_bound"])) +
+                             int(float(btn["lower_bound"]))) / 2
+            temp_name = list(dev_button_list.keys())[index]
+            cur_x = self.centre_x - \
+                math.sin(math.radians(-cur_angle)) * self.radius
+            cur_y = self.centre_y - \
+                math.cos(math.radians(cur_angle)) * self.radius
+
+            if "instance" not in btn:
+                name = list(dev_button_list.keys())[index]
+                edit_dev_pair = partial(self.edit_dev_config, name)
+                btn["instance"] = Button(self.canvas, text=name, width=2, height=2,
+                                         bd="0", command=edit_dev_pair)
+            self.place_dev_btn(btn["instance"], cur_x, cur_y)
+        print()
+
+    def place_dev_btn(self, instance, tar_x, tar_y):
+        instance.place(x=tar_x-24, y=tar_y-24)
+
+    def export_dev_json(self):
+        df = pd.DataFrame(dev_button_list).T
+        try:
+            df = df[["upper_bound", "lower_bound",
+                     "device_type", "ip_address"]].T
+        except:
+            print("No items so no column names")
+        df.to_json(CONFIG_FILE_NAME)
+
+    def load_dev_json(self):
+        global dev_button_list
+        files = os.listdir()
+        if CONFIG_FILE_NAME not in files:
+            return
+        with open(CONFIG_FILE_NAME) as fp:
+            dev_button_list = json.load(fp)
+        self.refresh_dev_bt_pos()
+
+
+if __name__ == "__main__":
+    inteface = InterfaceCanvas()
+    root.mainloop()
